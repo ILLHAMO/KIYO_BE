2dc5fdcf3fbc971769a3ee586b5a1b871f80ef75
jemin <jemin03120111@gmail.com>
2022-06-14 01:08:44 +0900

    Initial commit

---
+++ src/main/java/project/kiyobackend/KiyobackendApplication.java
@@ -0,0 +1,13 @@
+package project.kiyobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiyobackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiyobackendApplication.class, args);
+    }
+
+}
