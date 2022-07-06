edba6175e079eddd4456b33a58b2718f07edee68
jemin <jemin03120111@gmail.com>
2022-07-06 20:25:14 +0900

    test

--- src/test/java/project/kiyobackend/store/StoreControllerTest.java
+++ src/test/java/project/kiyobackend/store/StoreControllerTest.java
@@ -53,25 +53,25 @@ public class StoreControllerTest {
                 .apply(SecurityMockMvcConfigurers.springSecurity())
                 .build();
 
-        for(int i = 0; i < 100; i++)
-        {
-            storeRepository.save(new Store("상점"+i,
-                    "010-2757-2345",
-                    new Comment("simple", "detail"),
-                    "20:00", true,
-                    Arrays.asList(1L, 2L),
-                    Arrays.asList(3L, 4L)));
-        }
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserPrincipal principal = (UserPrincipal)authentication.getPrincipal();
-        // 현재 사용자 정보
-        User user = principal.getUser();
-
-        for(int j = 0; j < 30; j++)
-        {
-            bookmarkService.addLike(user,2L*j+1l);
-        }
+//        for(int i = 0; i < 100; i++)
+//        {
+//            storeRepository.save(new Store("상점"+i,
+//                    "010-2757-2345",
+//                    new Comment("simple", "detail"),
+//                    "20:00", true,
+//                    Arrays.asList(1L, 2L),
+//                    Arrays.asList(3L, 4L)));
+//        }
+//
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        UserPrincipal principal = (UserPrincipal)authentication.getPrincipal();
+//        // 현재 사용자 정보
+//        User user = principal.getUser();
+//
+//        for(int j = 0; j < 30; j++)
+//        {
+//            bookmarkService.addLike(user,2L*j+1l);
+//        }
     }
 
     @AfterEach
@@ -87,7 +87,7 @@ public class StoreControllerTest {
   @Transactional
   public void test() throws Exception
   {
-      mvc.perform(MockMvcRequestBuilders.get("/api/stores?lastStoreId=40&size=30")
+      mvc.perform(MockMvcRequestBuilders.get("/api/stores?lastStoreId=20&size=10")
               .contentType(MediaType.APPLICATION_JSON))
               .andExpect(MockMvcResultMatchers.status().isOk())
               .andDo(MockMvcResultHandlers.print());
