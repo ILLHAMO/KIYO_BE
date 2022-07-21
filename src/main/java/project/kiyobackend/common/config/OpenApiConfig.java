eaa35a935d6c3ae73af0339dd0238ada1f225bdc
jemin <jemin03120111@gmail.com>
2022-07-21 12:28:48 +0900

    fix: swagger server 설정 추가

--- src/main/java/project/kiyobackend/common/config/OpenApiConfig.java
+++ src/main/java/project/kiyobackend/common/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,11 +24,14 @@ public class OpenApiConfig {
                 .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER).name("Authorization");
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-
-
+        Server server1 = new Server();
+        server1.setUrl("https://www.jmsteady.net");
+        Server server2 = new Server();
+        server2.setUrl("http://localhost:8080");
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearerAuth",securityScheme))
                 .security(Arrays.asList(securityRequirement))
+                .servers(Arrays.asList(server1,server2))
                 .info(info);
     }
 
