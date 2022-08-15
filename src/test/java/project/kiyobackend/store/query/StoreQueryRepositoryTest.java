35a427d7e720977c2c0446d3375d7b6a1939674e
jemin <jemin03120111@gmail.com>
2022-08-15 17:27:09 +0900

    test: github actions 수정

--- src/test/java/project/kiyobackend/store/query/StoreQueryRepositoryTest.java
+++ src/test/java/project/kiyobackend/store/query/StoreQueryRepositoryTest.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
 class StoreQueryRepositoryTest {
 
     @Test
