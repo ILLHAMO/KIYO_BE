8c01541af43990a740c2aa99fcbd120a777596e0
jemin <jemin03120111@gmail.com>
2022-08-17 18:16:54 +0900

    test: RestAssure 추가

--- src/test/java/project/kiyobackend/store/adapter/presentation/StoreControllerTest.java
+++ src/test/java/project/kiyobackend/store/adapter/presentation/StoreControllerTest.java
@@ -14,9 +14,9 @@ class StoreControllerTest {
     @DisplayName("lastStoreId와 size가 주어졌을때 no-offset 방식으로 페이징 잘 되는지 테스트")
     void main_page()
     {
-        get("/api/stores?size=6&lastStoreId=7").then().body("content[0].id", equalTo(6));
-        get("/api/stores?size=6&lastStoreId=7").then().body("content[5].id", equalTo(1));
-        get("/api/stores?size=6&lastStoreId=7").then().body("last", equalTo(true));
+        get("http://localhost:8080/api/stores?size=6&lastStoreId=7").then().body("content[0].id", equalTo(6));
+        get("http://localhost:8080/api/stores?size=6&lastStoreId=7").then().body("content[5].id", equalTo(1));
+        get("http://localhost:8080/api/stores?size=6&lastStoreId=7").then().body("last", equalTo(true));
     }
 
 }
